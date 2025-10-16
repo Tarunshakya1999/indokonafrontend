a67aec2b362c327e7c41b6169e6a15da2736ddfe
TarunShakya <shakyatarun32@gmail.com>
2025-10-16 16:05:37 +0530

    Ne8968g8rrrrr8

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
             to="/"
           >
             <i className="fa-solid fa-lock me-2"></i>
-            Indokona <span style={{color:"yellow"}}>.com</span>
+            Indokona <span className="text-warning">.com</span>
             {/* <span style={{ color: "#f1c40f", marginLeft: "6px" }}>FinTech</span> */}
           </Link>
 
