b91d97b3cd2982a2250cacd2be69cdaf0ba9a0aa
TarunShakya <shakyatarun32@gmail.com>
2025-11-23 14:32:58 +0530

    jkkkkk jjjkk j kkkk jjj

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -13,7 +13,7 @@ export default function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("https://indokonabackend-1.onrender.com/api/register2/", form);
+      await axios.post("https://indokonabackend-1.onrender.com/register2/", form);
       setMsg("Signup successful! Now login.");
     } catch (error) {
       setMsg("Error: " + error.response.data.error);
