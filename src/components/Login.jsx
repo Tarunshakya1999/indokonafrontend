d6b2d74fc549958804a9d4b9b9fafad908b4a641
TarunShakya <shakyatarun32@gmail.com>
2025-11-23 15:56:55 +0530

    j 8887777 jj jj uuuu mm88  nnmm8 jjj

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,24 +5,32 @@ import { useNavigate } from "react-router-dom";
 export default function LoginPage() {
   const [form, setForm] = useState({ username: "", password: "" });
   const [msg, setMsg] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://indokonabackend-1.onrender.com/api/login2/", {
-        username: form.username,
-        password: form.password,
-      });
+      const res = await axios.post(
+        "https://indokonabackend-1.onrender.com/api/login2/",
+        {
+          username: form.username,
+          password: form.password,
+        }
+      );
 
+      // store tokens
       localStorage.setItem("access", res.data.access);
       localStorage.setItem("refresh", res.data.refresh);
 
+      // store username
+      localStorage.setItem("username", form.username);
+
       setMsg("Login successful!");
-      navigate("/wall")
+      navigate("/wall");
+
     } catch (err) {
-      setMsg("Invalid email or password");
+      setMsg("Invalid username or password");
     }
   };
 
@@ -35,14 +43,18 @@ export default function LoginPage() {
         <input
           className="form-control mb-2"
           placeholder="Username"
-          onChange={(e) => setForm({ ...form, username: e.target.value })}
+          onChange={(e) =>
+            setForm({ ...form, username: e.target.value })
+          }
         />
 
         <input
           className="form-control mb-2"
           placeholder="Password"
           type="password"
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          onChange={(e) =>
+            setForm({ ...form, password: e.target.value })
+          }
         />
 
         <button className="btn btn-success w-100">Login</button>
