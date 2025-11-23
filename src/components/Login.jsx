693e3cdb7f5888a829e35a17138a80757ee35940
TarunShakya <shakyatarun32@gmail.com>
2025-11-23 15:24:11 +0530

    jkkkkk jjjkk j kkkk jjj 77777 mm jj nnn mm 00 nnn mm

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,26 +3,29 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [form, setForm] = useState({ username: "", password: "" });
   const [msg, setMsg] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://indokonabackend-1.onrender.com/api/login2/", {
-        username: form.email,
-        password: form.password,
-      });
+      const res = await axios.post(
+        "https://indokonabackend-1.onrender.com/api/login2/",
+        {
+          username: form.username,
+          password: form.password,
+        }
+      );
 
       localStorage.setItem("access", res.data.access);
       localStorage.setItem("refresh", res.data.refresh);
 
       setMsg("Login successful!");
-      navigate("/wall")
+      navigate("/wall");
     } catch (err) {
-      setMsg("Invalid email or password");
+      setMsg("Invalid username or password");
     }
   };
 
@@ -34,15 +37,19 @@ export default function LoginPage() {
       <form onSubmit={handleSubmit}>
         <input
           className="form-control mb-2"
-          placeholder="Email"
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          placeholder="Username"
+          onChange={(e) =>
+            setForm({ ...form, username: e.target.value })
+          }
         />
 
         <input
           className="form-control mb-2"
           placeholder="Password"
           type="password"
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          onChange={(e) =>
+            setForm({ ...form, password: e.target.value })
+          }
         />
 
         <button className="btn btn-success w-100">Login</button>
