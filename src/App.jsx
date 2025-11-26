36a134df59a612137d1c9821ac8d859ab22978f4
TarunShakya <shakyatarun32@gmail.com>
2025-11-26 14:20:26 +0530

    fix hhh7 nnn n nn hh nnh jj887666 nn nn bbb

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ import ForgotPasswordPage from './components/ForgotPasswordPage';
 import ResetPasswordPage from './components/ResetPasswordPage';
 import MsmeForm from "./MsmeForm";
 import ExploreBusinessForms from "./Explore";
-import FssaiForm from "./src/Forms/FassaiForm";
+
 
 
 //Acadmic Files
@@ -127,7 +127,7 @@ function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
 
         {/* <Route path="/login2" element={<Login />} /> */}
-        <Route path="/fssaiform" element={<FssaiForm/>}/>
+        {/* <Route path="/fssaiform" element={<FssaiForm/>}/> */}
       </Routes>
     </BrowserRouter>
   );
