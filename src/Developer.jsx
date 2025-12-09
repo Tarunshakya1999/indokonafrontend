a504ec245aeb12013611a0180071f73d1766d1c1
TarunShakya <shakyatarun32@gmail.com>
2025-12-09 14:32:43 +0530

    Mail systemkkkm bbbjjjm  jj j87 nn6

--- src/Developer.jsx
+++ src/Developer.jsx
@@ -669,9 +669,9 @@ export default function TechServicesLanding() {
         <div className="container text-center">
           <div className="d-flex align-items-center justify-content-center mb-2">
             <Code size={20} color="#a78bfa" />
-            <span className="ms-2 fw-bold">Indokona Tech Solutions</span> <br />
+            <span className="ms-2 fw-bold">Indokona Tech Solutions</span> 
+            </div>
             <Link to ="/terms&conditions" className="text-light text-decoration-none">Terms & Conditions</Link>
-          </div>
           <p className="ts-muted mb-1">Building digital solutions that transform businesses</p>
           <small className="text-light">© {new Date().getFullYear()} Indokona TechSolutions. All rights reserved.</small>
         </div>
